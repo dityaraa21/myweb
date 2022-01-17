53d5ea0427c33a7a24cf27032a881bd0c45c09ee
dityaraa21 <adityarahman453@gmail.com>
2022-01-17 15:46:29 +0700

    membuat user admin, view admin dan view includes

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+  <head>
+    @include('includes.head')
+  </head>
+  <body>
+      @include('includes.navbar')
+      <div class="wrap container">
+        <div id="main" class="row">
+          <div class="col-md-12">
+            @include('pages.header')
+          </div>
+        </div>
+      </div>
+      @include('includes.footer')
+  </body>
+</html>
